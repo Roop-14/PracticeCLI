4b98b87b649d2fb3c184517b6c524343c4b06b64
Roopam Mondal <roopammondal14@gmail.com>
2025-03-08 22:00:35 +0530

    test 2 modified again

--- test1.java
+++ test1.java
@@ -1,11 +1,11 @@
 public class test1{
 
 public void m2(){
-System.out,println("m2");
+System.out.println("m2");
 }
 
 public static void main(String[] args){
-System.out.println("	Roopam");
+System.out.println("Roopam");
 
 }
 }
